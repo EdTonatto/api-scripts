4298d77c11666dae578fc2fb89264d6133ea3a9a
Eduardo <edtonatto@gmail.com>
2020-02-12 12:36:46 -0300

    :heavy_plus_sign: feat(mongoose): add mongo connection with mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 require('dotenv/config')
 const express = require('express');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 const app = express()
 app.use(express.json())
