53f316ca63bbee50cf2e3693b3b570c7b407f8e9
Eduardo <edtonatto@gmail.com>
2020-02-12 13:03:50 -0300

    :sparkles: feat(script): add post method

--- src/controllers/ScriptController.js
+++ src/controllers/ScriptController.js
@@ -5,5 +5,17 @@ module.exports = {
         const scripts = await Script.find()
 
         return res.json(scripts)
+    },
+
+    async store(req, res){
+        const {name, content, author} = req.body
+        
+        const script = await Script.create({
+            name, 
+            content, 
+            author
+        })
+
+        return res.json(script)
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const routes = Router()
 
 const ScripController = require('./controllers/ScriptController')
 routes.get('/scripts', ScripController.index)
+routes.post('/scripts', ScripController.store)
 
 module.exports = routes
