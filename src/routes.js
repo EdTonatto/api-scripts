f1cf9535d6aa25dc77b0a4bbfe6bbb3ef6b48eab
Eduardo Tonatto <edtonatto@gmail.com>
2020-02-12 22:20:52 -0300

    :sparkles: feat(search): added route to search
    Added route to search scripts by name or author.

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,7 @@ routes.post('/scripts', ScriptController.store)
 routes.delete('/scripts/:id', ScriptController.destroy)
 routes.post('/scripts/:id', ScriptController.update)
 
+const SearchController = require('./controllers/SearchController')
+routes.get('/search', SearchController.index)
 
 module.exports = routes
